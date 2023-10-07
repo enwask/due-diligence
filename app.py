168b0262c2c35a9bedd288b10b968edd9fe835f2
enwask <enwask@ucf.edu>
2023-10-07 19:35:31 -0400

    Finish basic app layout and styling

--- app.py
+++ app.py
@@ -4,8 +4,18 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():  # put application's code here
+def index():
     return render_template('index.html')
+
+
+@app.route('/lists')
+def lists():
+    return render_template('lists.html')
+
+
+@app.route('/history')
+def history():
+    return render_template('history.html')
 
 
 if __name__ == '__main__':
