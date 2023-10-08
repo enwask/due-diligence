6809decf6533723e9c89df8ec3b92b23fafd4577
enwask <enwask@ucf.edu>
2023-10-08 03:12:57 -0400

    Add Oxylabs secrets

--- api/secrets.py
+++ api/secrets.py
@@ -1,16 +1,16 @@
 from google.cloud import secretmanager as mgr
 from google_crc32c import Checksum
-from os import getenv as env
 
 # Constants
 __GOOGLE_CLOUD_PROJECT_ID = "knighthacks23"
 __OPENAI_KEY_ID = "openai_key"
 __GOOGLE_SEARCH_KEY_ID = "google_search_key"
+__OXYLABS_USER_ID = "oxylabs_user"
+__OXYLABS_KEY_ID = "oxylabs_key"
 
 
 # Fetch a secret from Google Secret Manager
-def __get_secret(secret_id: str, version_id: str = "latest",
-                 enc: str = "UTF-8") -> str | mgr.AccessSecretVersionResponse:
+def __get_secret(secret_id: str, version_id: str = "latest") -> str:
     client = mgr.SecretManagerServiceClient()
     name = f"projects/{__GOOGLE_CLOUD_PROJECT_ID}/secrets/{secret_id}/versions/{version_id}"
     response = client.access_secret_version(request={"name": name})
@@ -20,28 +20,14 @@ def __get_secret(secret_id: str, version_id: str = "latest",
     checksum.update(response.payload.data)
     if int(checksum.hexdigest(), 16) != response.payload.data_crc32c:
         print("Invalid checksum when retrieving secret")
-        return response
+        return str(response)
 
-    return response.payload.data.decode(enc)
-
-
-# Fetch the OpenAI key from Google Secret Manager
-def __get_secret_openai(version_id: str = "latest"):
-    return __get_secret(__OPENAI_KEY_ID, version_id)
+    return response.payload.data.decode("UTF-8")
 
 
-# Fetch the Google Search key, from .env (if running locally) or from the Secret Manager
-def __get_secret_google_search(version_id: str = "latest"):
-    # If running locally, use the key in the .env file
-    if env("SERVER_SOFTWARE", "Dev").startswith("Dev"):
-        print("Using local Google Search key")
-        return env("GOOGLE_SEARCH_API_KEY")
+openai_key: str = __get_secret(__OPENAI_KEY_ID)
+google_search_key: str = __get_secret(__GOOGLE_SEARCH_KEY_ID)
+oxylabs_user: str = __get_secret(__OXYLABS_USER_ID)
+oxylabs_key: str = __get_secret(__OXYLABS_KEY_ID)
 
-    # Otherwise fetch the key from the Secret Manager
-    return __get_secret(__GOOGLE_SEARCH_KEY_ID, version_id)
-
-
-openai_key: str = __get_secret_openai()
-google_search_key: str = __get_secret_google_search()
-
-__all__ = ["openai_key", "google_search_key"]
+__all__ = ["openai_key", "google_search_key", "oxylabs_user", "oxylabs_key"]
