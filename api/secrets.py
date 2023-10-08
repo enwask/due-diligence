3d256772c70c638fa350044ca26b36ca80c6e656
enwask <enwask@ucf.edu>
2023-10-07 21:29:21 -0400

    Add secret manager SDK, update runtime setting

---
+++ api/secrets.py
@@ -0,0 +1,24 @@
+from google.cloud import secretmanager as mgr
+from google_crc32c import Checksum
+
+PROJECT_ID = "knighthacks23"
+OPENAI_KEY_ID = "openai_key"
+
+
+def get_secret(secret_id: str, version_id: str = "latest", enc: str = "UTF-8") -> str | mgr.AccessSecretVersionResponse:
+    client = mgr.SecretManagerServiceClient()
+    name = f"projects/{PROJECT_ID}/secrets/{secret_id}/versions/{version_id}"
+    response = client.access_secret_version(request={"name": name})
+
+    # Verify checksum
+    checksum = Checksum()
+    checksum.update(response.payload.data)
+    if int(checksum.hexdigest(), 16) != response.payload.data_crc32c:
+        print("Invalid checksum when retrieving secret")
+        return response
+
+    return response.payload.data.decode(enc)
+
+
+def get_secret_openai(version_id: str = "latest"):
+    return get_secret(OPENAI_KEY_ID, version_id)
