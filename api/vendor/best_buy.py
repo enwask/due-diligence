0046ce5f163a17215f2199dd2bc296b2ff13359a
enwask <enwask@ucf.edu>
2023-11-02 18:18:12 -0400

    Skip duplicates from Best Buy product API

--- api/vendor/best_buy.py
+++ api/vendor/best_buy.py
@@ -22,13 +22,14 @@ class BestBuy(ProductBase):
     def search_products(self, query: str, num: int = 50, **kwargs: any) -> list[Product]:
         # https://bestbuyapis.github.io/bby-query-builder/#/productSearch
         kwargs.update({
-            "sort": "salePrice.dsc",
-            "show": "sku,name,longDescription,salePrice,url,thumbnailImage,features.feature",
-            "pageSize": num,
+            "sort": "salePrice.dsc",  # TODO: Better product selection (maybe request data for a few price ranges)
+            "show": "sku,name,longDescription,salePrice,url,thumbnailImage,modelNumber,features.feature",
+            "pageSize": num + 10,  # Add some padding in case we get duplicates
             "format": "json"
         })
 
         products = []
+        models = set()
         # noinspection PyBroadException
         try:
             # Construct the search query
@@ -39,6 +40,20 @@ class BestBuy(ProductBase):
 
             # Parse the results
             for item in res["products"]:
+                # If we have enough products, terminate
+                if len(products) == num:
+                    break
+
+                # Get the product model
+                model = item["modelNumber"]
+
+                # If this is a duplicate product, skip it
+                if model in models:
+                    continue
+
+                # Add the model number to the set
+                models.add(model)
+
                 # Add features to the product description
                 desc = item["longDescription"]
                 for feat in item["features"]:
