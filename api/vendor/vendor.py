aea7cfab6e67ba02a608ac92c64c8bb13256199b
enwask <enwask@ucf.edu>
2023-11-02 11:59:19 -0400

    Add search page

--- api/vendor/vendor.py
+++ api/vendor/vendor.py
@@ -2,6 +2,22 @@ from enum import Enum
 
 
 class Vendor(Enum):
+    # AMAZON = "amazon"
     BEST_BUY = "best_buy"
     EBAY = "ebay"
     ETSY = "etsy"
+    GOOGLE_SHOPPING = "google_shopping"
+    WALMART = "walmart"
+
+    def __str__(self) -> str:
+        return self.value
+
+
+vendor_info: dict[Vendor, str | tuple[str, bool, str | None]] = {
+    # Vendor.AMAZON: ("Amazon", False, "Coming soon"),
+    Vendor.BEST_BUY: ("Best Buy", True, None),
+    Vendor.EBAY: ("eBay", True, None),
+    Vendor.ETSY: ("Etsy", True, None),
+    Vendor.GOOGLE_SHOPPING: ("Google Shopping", False, "Legacy"),
+    Vendor.WALMART: ("Walmart", False, "Coming soon")
+}
--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from starlette.staticfiles import StaticFiles
 
 from api.llm import fix_product_names_async, collect_features_async
 from api.response import LoadStatus, ProductData, LoadError
-from api.vendor import Vendor, vendors
+from api.vendor import Vendor, vendors, vendor_info
 from api.website.pug import PugRenderer
 
 # Create FastAPI app and mount static files
@@ -19,9 +19,8 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory=abspath("static")), name="static")
 
 # Create template renderer and default context
-templates = PugRenderer(abspath("static/templates"))
-default_context = {
-}
+templates = PugRenderer(abspath("static/pug"))
+default_context = {}
 
 
 # Decorator for asynchronous template routes
@@ -49,12 +48,12 @@ TemplateResponse: type = tuple[str, dict[str, any]]
 # 404 page
 @cache
 @app.exception_handler(404)
-# @template("/404")
 async def error_404(request: Request, _: Exception):
     return await handle_http_exception(request,
                                        HTTPException(status_code=404, detail="We couldn't find the requested page."))
 
 
+# HTTP exception handler
 @app.exception_handler(HTTPException)
 async def handle_http_exception(request: Request, exception: HTTPException) -> Response:
     accepts = request.headers.get("accept", "")
@@ -70,23 +69,46 @@ async def handle_http_exception(request: Request, exception: HTTPException) -> R
 # Product comparison endpoint
 @app.get("/compare/{query}")
 async def compare(query: str, vendor: Vendor = Vendor.BEST_BUY):
-    async def gen():
+    async def fun():
         try:
             # Search for products
             yield LoadStatus("Searching for products...").str()
             products = vendors[vendor].search_products(quote(query), 5)
 
+            # Fix product names
             yield LoadStatus("Fixing product names...").str()
             await fix_product_names_async(products)
 
+            # Get product features
             yield LoadStatus("Extracting features...").str()
             num_sent = 0
             async for index, feat in collect_features_async(products):
+                # Send product data
                 yield LoadStatus("Extracting features... (%d/%d)" % (num_sent + 1, len(products))).str()
                 yield ProductData(products[index], feat).str()
                 num_sent += 1
+
+        # Handle error in feature loading
         except Exception as e:
             yield LoadError("Internal error: " + str(e))
 
+    # Return the generator function as a streaming response
     headers = {"X-Content-Type-Options": "nosniff"}
-    return StreamingResponse(gen(), media_type="text/json", headers=headers)
+    return StreamingResponse(fun(), media_type="text/json", headers=headers)
+
+
+# Home page / product comparison frontend
+@cache
+@template("/")
+async def search() -> TemplateResponse:
+    return "search.pug", {
+        "title": "Search",
+        "vendors": [
+            {
+                "id": vendor.value,
+                "name": vendor_info[vendor][0],
+                "enabled": vendor_info[vendor][1],
+                "badge": vendor_info[vendor][2] or ""
+            } for vendor in Vendor
+        ]
+    }
