3e2a5f0f3a478089fab52c0bf40b298472fdefb1
enwask <enwask@ucf.edu>
2023-11-15 19:13:40 -0500

    Asynchronously collect names and features

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from functools import cache
 from os.path import abspath
 from urllib.parse import quote
 
+from aiostream.stream import merge as merge_streams
 from fastapi import FastAPI, HTTPException
 from fastapi.exception_handlers import http_exception_handler as fastapi_handle_http_exception
 from fastapi.responses import StreamingResponse
@@ -10,8 +11,8 @@ from starlette.requests import Request
 from starlette.responses import HTMLResponse, Response
 from starlette.staticfiles import StaticFiles
 
-from api.llm import fix_product_names_async, collect_features_async
-from api.response import LoadStatus, ProductData, LoadError
+from api.llm import collect_names_async, collect_features_async
+from api.response import LoadStatus, ProductData, LoadError, json_stream
 from api.vendor import Vendor, vendors, vendor_info
 from api.website.pug import PugRenderer
 
@@ -26,18 +27,18 @@ default_context = {}
 
 # Decorator for asynchronous template routes
 def template(route: str) -> callable:
-    def wrapper(func: callable) -> callable:
+    def wrapper(fun: callable) -> callable:
         @app.get(route, response_class=HTMLResponse)
         async def template_route():
             # Add returned context to a copy of the default context
-            res, add = await func()
+            res, add = await fun()
             ctx = default_context.copy()
             ctx.update(add)
 
             # Render template and return response
             return templates.response(res, ctx)
 
-        return func
+        return fun
 
     return wrapper
 
@@ -70,29 +71,54 @@ async def handle_http_exception(request: Request, exception: HTTPException) -> R
 # Product comparison endpoint
 @app.get("/compare/{query}")
 async def compare(query: str, vendor: Vendor = Vendor.BEST_BUY, num: int = 10):
+    num = min(20, max(1, num))
+
+    @json_stream
     async def fun():
         try:
             # Search for products
-            yield LoadStatus("Searching for products...").str()
+            yield LoadStatus("Searching products...").json()
             products = vendors[vendor].search_products(quote(query), num)
 
-            # Fix product names
-            yield LoadStatus("Fixing product names...").str()
-            await fix_product_names_async(products)
+            # Get product features and fix names
+            # TODO: Move this to another module
+            yield LoadStatus("Extracting features...").json()
+            name_fixed = [False for _ in products]
+            features: list[None | dict[str, any]] = [None for _ in products]
 
-            # Get product features
-            yield LoadStatus("Extracting features...").str()
+            # Operate on each result as it comes in
+            streamer = merge_streams(collect_names_async(products), collect_features_async(products))
             num_sent = 0
-            async for index, feat in collect_features_async(products):
-                # Send product data
-                yield ("[" +
-                       LoadStatus("Extracting features... (%d/%d)" % (num_sent + 1, len(products))).str() + ", " +
-                       ProductData(products[index], feat).str() + "]")
-                num_sent += 1
+            async with streamer.stream() as stream:
+                async for res in stream:
+                    index: int
+                    feat: dict[str, any]
+
+                    if isinstance(res, int):
+                        # Product name fixed
+                        name_fixed[res] = True
+
+                        # If features are not ready, end iteration
+                        if features[res] is None:
+                            continue
+                    else:
+                        # Product features extracted
+                        index, feat = res
+                        features[index] = feat
+
+                        # If name is not ready, end iteration
+                        if not name_fixed[index]:
+                            continue
+
+                    # Send product data
+                    yield [
+                        LoadStatus("Extracting features... (%d/%d)" % (num_sent + 1, len(products))).json(),
+                        ProductData(products[index], feat).json()]
+                    num_sent += 1
 
         # Handle error in feature loading
         except Exception as e:
-            yield LoadError("Internal error: " + str(e)).str()
+            yield LoadError("Internal error: " + str(e)).json()
 
     # Return the generator function as a streaming response
     headers = {"X-Content-Type-Options": "nosniff"}
@@ -108,26 +134,53 @@ async def tune(query: str, vendor: Vendor = Vendor.BEST_BUY, num: int = 10):
             yield "<p>Searching for products...</p>"
             products = vendors[vendor].search_products(quote(query), num)
 
-            # Fix product names
-            yield "<p>Fixing product names...</p>"
-            await fix_product_names_async(products)
+            # Fix product names FIXME: Use the thing below with stream merge
+            # yield "<p>Fixing product names...</p>"
+            # await gather_names_async(products)
 
             # Get product features
             yield "<p>Extracting features...</p>"
-            num_sent = 0
-            async for index, feat in collect_features_async(products):
-                # Send product data
-                br = '\n'  # No backslashes in f-string
-                yield f"<textarea style='width:60%;height:300px;'>{products[index].desc.replace(br, ' ')}</textarea><br/><br/>"
-                yield f"<textarea style='width:60%;height:300px;' id='i{num_sent}'>{json.dumps(ProductData(products[index], feat).json()['features'], indent=2)}</textarea><br/><br/>"
-                yield f"<button id='b{num_sent}' style='padding:10px 5px;'>Remove linebreaks</button><br/><br/><br/>"
-                yield f"<script>let el{num_sent}=document.querySelector('#b{num_sent}');el{num_sent}.onclick=()=>{{i{num_sent}.value=i{num_sent}.value.replaceAll('\\n',' ').replaceAll('   ',' ');}};</script>"
+            name_fixed = [False for _ in products]
+            features: list[None | dict[str, any]] = [None for _ in products]
 
-                num_sent += 1
+            # Operate on each result as it comes in
+            streamer = merge_streams(collect_names_async(products), collect_features_async(products))
+            num_sent = 0
+            async with streamer.stream() as stream:
+                async for res in stream:
+                    index: int = -1
+                    feat: dict[str, any]
+
+                    if isinstance(res, int):
+                        # Product name fixed
+                        name_fixed[res] = True
+                        # products[]
+
+                        # If features are not ready, end iteration
+                        if features[res] is None:
+                            continue
+                    else:
+                        # Product features extracted
+                        index, feat = res
+                        features[index] = feat
+
+                        # If name is not ready, end iteration
+                        if not name_fixed[index]:
+                            continue
+
+                    # Send response chunk
+                    br = '\n'  # No backslashes in f-string
+                    yield f"<h2>{products[index].name}</h2>"
+                    yield f"<textarea style='width:60%;height:300px;'>{products[index].desc.replace(br, ' ')}</textarea><br/><br/>"
+                    yield f"<textarea style='width:60%;height:300px;' id='i{num_sent}'>{json.dumps(ProductData(products[index], feat).json()['features'], indent=2)}</textarea><br/><br/>"
+                    yield f"<button id='b{num_sent}' style='padding:10px 5px;'>Remove linebreaks</button><br/><br/><br/>"
+                    yield f"<script>let el{num_sent}=document.querySelector('#b{num_sent}');el{num_sent}.onclick=()=>{{i{num_sent}.value=i{num_sent}.value.replaceAll('\\n',' ').replaceAll('   ',' ');}};</script>"
+
+                    num_sent += 1
 
         # Handle error in feature loading
         except Exception as e:
-            yield LoadError("Internal error: " + str(e)).str()
+            yield LoadError("Internal error: " + str(e)).json()
 
     # Return the generator function as a streaming response
     headers = {"X-Content-Type-Options": "nosniff"}
